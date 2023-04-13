f251c61c50da56c4dce91a34f2e49ae7ab52ebb3
lourdesConti <72649170@continental.edu.pe>
2023-04-13 01:45:39 -0500

    agregue funcion resta

--- tarea1.py
+++ tarea1.py
@@ -1,4 +1,17 @@
-
+def suma (x,y):
+        try :
+            s=x+y 
+        except TypeError :
+            print ("lo siento ,solo son permitidos numeros numericos ")
+        else : 
+             return s
+def resta (m,n):
+        try :
+            s=m-n 
+        except TypeError :
+            print ("lo siento ,solo son permitidos numeros numericos ")
+        else : 
+             return s    
 
 
 print("hola Profesor")
@@ -18,13 +31,15 @@ print (menu)
 
 opcion= input ("ingrese un numero: ")
 if opcion == '1' :
-    def suma (x,y):
-        try :
-            s=x+y 
-        except TypeError :
-            print ("lo siento ,solo son permitidos numeros numericos ")
+    x=float (input ("ingrese numero 1 : "))
+    y=float (input ("ingrese numero 2 : "))
+    resul = suma (x,y)
+    print(resul)
 elif opcion == '2' :
-    pass
+   m=float (input ("ingrese numero 1 : "))
+   n=float (input ("ingrese numero 2 : "))
+   resul = resta (m,n)
+   print(resul)
 elif opcion == '3':
     pass
 elif opcion == '4':

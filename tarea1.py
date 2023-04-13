6467790f1dd1a32953008990fb9e47ce3502d7a5
ContiClara <73904391@continental.edu.pe"git config --global user.email 73904391@continental.edu.pegit config --global user.email '73904391@continental.edu.pe>
2023-04-13 01:41:37 -0500

    Agregue multiplcacion

--- tarea1.py
+++ tarea1.py
@@ -7,6 +7,19 @@ def divide(x, y ):
             print ("Lo siento, solo son permitidos valores numericos")
         else:
             return z
+        finally: 
+             print("Fin")
+def multiplica(a, b ):
+        try :
+            m= a*b
+        except ZeroDivisionError:
+            print ("Lo siento està dividiendo entre cer")
+        except TypeError:
+            print ("Lo siento, solo son permitidos valores numericos")
+        else:
+            return m
+        finally: 
+             print("Fin")
 
 
 print("hola  mundo soy clara")
@@ -30,7 +43,10 @@ if opcion == '1' :
 elif opcion == '2' :
     pass
 elif opcion == '3':
-    pass
+    a=float (input ("ingrese numero 1: "))
+    b=float(input("Ingrese numero 2: ")) 
+    result = multiplica(a,b)
+    print (result)
 elif opcion == '4':
     x=float (input ("ingrese numero 1: "))
     y=float(input("Ingrese numero 2: ")) 

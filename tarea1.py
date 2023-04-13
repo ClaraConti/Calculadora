9b911397971c9a6ef3e93e8721185cb4b3f7b0ff
lourdesConti <72649170@continental.edu.pe>
2023-04-13 00:48:15 -0500

    nuevo cambio

--- tarea1.py
+++ tarea1.py
@@ -1,8 +1,8 @@
 
 
 
-print("hola mundo")
-print("Hola soy")
+print("hola Profesor")
+print("Hola soy su alumna")
 
 menu=""" Bienvenido a nuestra calculadora, 
 seleccione un nùmero de 1 al 4: 
@@ -18,7 +18,11 @@ print (menu)
 
 opcion= input ("ingrese un numero: ")
 if opcion == '1' :
-    pass
+    def suma (x,y):
+        try :
+            s=x+y 
+        except TypeError :
+            print ("lo siento ,solo son permitidos numeros numericos ")
 elif opcion == '2' :
     pass
 elif opcion == '3':

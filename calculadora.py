04b02cf51c84dbd56262b6f2604693ee08121337
ContiClara <73904391@continental.edu.pe"git config --global user.email 73904391@continental.edu.pegit config --global user.email '73904391@continental.edu.pe>
2023-04-13 02:40:48 -0500

    Base para la aclucladora

---
+++ calculadora.py
@@ -0,0 +1,43 @@
+
+
+
+print("hola  mundo soy clara")
+print("Hola soy")
+
+menu=""" Bienvenido a nuestra calculadora, 
+seleccione un nùmero de 1 al 4: 
+
+[1] Sumar
+[2] Restar
+[3] Multiplicar
+[4] Dividir
+
+"""
+
+print (menu)
+
+opcion= input ("ingrese un numero: ")
+if opcion == '1' :
+    pass
+elif opcion == '2' :
+    pass
+elif opcion == '3':
+    pass
+elif opcion == '4':
+    pass
+    
+    
+else :
+    print ("Debes de digitar un numero entre el 1 y 4")
+
+
+
+
+
+
+
+
+
+ 
+
+
